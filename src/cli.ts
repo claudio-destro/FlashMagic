e768837398d5e9bc5c86777fa27843f19db54823
Claudio Destro <claudio.destro@gmail.com>
2016-01-19 08:13:36 +0100

    Lint.

--- src/cli.ts
+++ src/cli.ts
@@ -8,70 +8,67 @@ import * as fs from 'fs';
 var defaultComPort = '/dev/tty.usbmodemFD131';
 
 program
-	.description('flash files at given addresses')
-	.option('-p, --port [port]', `serial port [${defaultComPort}]`, defaultComPort);
+  .option('-p, --port [port]', `serial port [${defaultComPort}]`, defaultComPort);
 
-program
-	.command('flash <file>')
-	.description('flash binary file')
-	.option('-A, --address <address>', 'flash address', parseInt)
-	.action((file, cmd) => {
-		InSystemProgramming.makeAndOpen(program['port'])
-			.then((isp) => programFile(isp, file, cmd.address).then(() => isp.close()))
-			.catch(catchError);
-	});
+program.command('flash <file>')
+  .description('program file')
+  .option('-A, --address <address>', 'flash address', parseInt)
+  .action((file, cmd) => {
+    InSystemProgramming.makeAndOpen(program['port'])
+      .then((isp) => programFile(isp, file, cmd.address).then(() => isp.close()))
+      .catch(catchError);
+  });
 
-program
-	.command('ping')
-	.description('ping the specified device')
-	.action(() => {
-		InSystemProgramming.makeAndOpen(program['port'])
-			.then((isp) => pingDevice(isp))
-			.catch(catchError);
-	});
+program.command('ping')
+  .description('ping device')
+  .action(() => {
+    InSystemProgramming.makeAndOpen(program['port'])
+      .then((isp) => pingDevice(isp))
+      .catch(catchError);
+  });
 
 program.parse(process.argv);
 
 if (program.args.length === 0) {
-	program.help();
+  program.help();
 }
 
 function programFile(isp: InSystemProgramming, path: string, address: number): Promise<void> {
-	return isp.sendUnlockCommand().then(() => {
-		let size = fs.statSync(path).size;
-		let count = 0;
-		let programmer = new LPCProgrammer(isp, address, size);
-		return new Promise<void>((resolve, reject) => {
-			let stream = fs.createReadStream(path);
-			programmer.program(stream)
-				.on('start', () => console.log(`About to flash ${size} bytes...`))
-				.on('chunk', buffer => count += buffer.length)
-				.on('error', error => reject(error))
-				.on('end', () => {
-					console.log(`${path}: ${count} bytes written`);
-					stream.close();
-					resolve();
-				});
-		});
-	});
+  return isp.sendUnlockCommand().then(() => {
+    let size = fs.statSync(path).size;
+    let count = 0;
+    let programmer = new LPCProgrammer(isp, address, size);
+    return new Promise<void>((resolve, reject) => {
+      let stream = fs.createReadStream(path);
+      programmer.program(stream)
+        .on('start', () => console.log(`About to flash ${size} bytes...`))
+        .on('chunk', buffer => count += buffer.length)
+        .on('error', error => reject(error))
+        .on('end', () => {
+          console.log(`${path}: ${count} bytes written`);
+          stream.close();
+          resolve();
+        });
+    });
+  });
 }
 
 function pingDevice(isp: InSystemProgramming): void {
-	let count = 0;
-	(function loop(): void {
-		let start = Date.now();
-		isp.sendUnlockCommand().then(() => {
-			console.log(`PING seq=${count++} time=${Date.now() - start} ms`);
-			setTimeout(loop, 1000);
-		}).catch(error => {
-			console.error(error);
-			setTimeout(loop, 1000);
-		});
-	})();
+  let count = 0;
+  (function loop(): void {
+    let start = Date.now();
+    isp.sendUnlockCommand().then(() => {
+      console.log(`PING seq=${count++} time=${Date.now() - start} ms`);
+      setTimeout(loop, 1000);
+    }).catch(error => {
+      console.error(error);
+      setTimeout(loop, 1000);
+    });
+  })();
 }
 
 function catchError(error: any): void {
-	let stack = error['stack'];
-	console.error(stack ? stack : error);
-	process.exit(1);
+  let stack = error['stack'];
+  console.error(stack ? stack : error);
+  process.exit(1);
 }
